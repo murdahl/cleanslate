e2cc4c8ea95cb6231292a154c1a5b8230b55bb16
Svein Halvor Halvorsen <svein@finkoslo.no>
2018-06-08 14:45:51 +0200

    Added an Xcode project

---
+++ ios/Clean Slate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clean Slate
+//
+//  Created by Svein Halvor Halvorsen on 08.06.2018.
+//  Copyright © 2018 Fink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
