ee173087c4307b05ad51d2f3054e8bb2d5f6dd41
Ole Magnus Urdahl <murdahl89@gmail.com>
2018-06-18 23:38:34 +0200

    Android app foundation

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
 
+app.get('/another-endpoint/', (req, res) => {
+  res.json({ anotherHello: 'World' });
+});
+
 app.listen(process.env.PORT || 3000);
